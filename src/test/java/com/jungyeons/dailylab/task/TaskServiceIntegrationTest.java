b41912dbfb7922c76b19a3fe37c0a65709863cdf
BaeJungYeon <81145288+jungyeons@users.noreply.github.com>
2026-07-14 16:10:59 +0900

    feat: add case insensitive task search

--- src/test/java/com/jungyeons/dailylab/task/TaskServiceIntegrationTest.java
+++ src/test/java/com/jungyeons/dailylab/task/TaskServiceIntegrationTest.java
@@ -47,6 +47,7 @@ class TaskServiceIntegrationTest {
 		assertThat(taskService.findAll(
 				TaskStatus.TODO,
 				TaskCategory.FEATURE,
+				null,
 				PageRequest.of(0, 20)
 		)).hasSize(1);
 
@@ -57,5 +58,23 @@ class TaskServiceIntegrationTest {
 		assertThat(completed.completedAt()).isNotNull();
 		assertThat(taskService.summary().done()).isEqualTo(1);
 		assertThat(taskService.summary().todo()).isZero();
+	}
+
+	@Test
+	void searchesTitlesAndDescriptionsWithoutCaseSensitivity() {
+		TaskResponse titleMatch = taskService.create(new CreateTaskRequest(
+				"Spring Boot migration", null, TaskCategory.FEATURE, 3, null
+		));
+		TaskResponse descriptionMatch = taskService.create(new CreateTaskRequest(
+				"Upgrade service", "Expose a REST endpoint", TaskCategory.FEATURE, 3, null
+		));
+		taskService.create(new CreateTaskRequest("Write release notes", "Document changes", TaskCategory.FEATURE, 3, null));
+
+		assertThat(taskService.findAll(null, null, "sPrInG", PageRequest.of(0, 20)))
+				.extracting(TaskResponse::id)
+				.containsExactly(titleMatch.id());
+		assertThat(taskService.findAll(null, null, "rest", PageRequest.of(0, 20)))
+				.extracting(TaskResponse::id)
+				.containsExactly(descriptionMatch.id());
 	}
 }
